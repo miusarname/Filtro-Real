5104c13353e9673985b4c719fca0850655ceb9bb
miusarname <oscarm.alvarezg@gmail.com>
2023-07-31 16:25:41 -0500

    feat: :sparkles: Se completa el punto 10
    Se completa el punto 10,.Listar los clientes con el DNI específico

--- routers/clientes.routes.ts
+++ routers/clientes.routes.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
 import { Connection } from "../middlewares/classes/connection.js";
 import { validateJWT } from "../middlewares/tokenValidation.js";
+import { Costumers } from "../storage/customers.js";
+import { validate } from "class-validator";
+import { plainToClass } from "class-transformer";
 
 export const clientes = express.Router();
 
@@ -38,6 +41,34 @@ clientes.get("/", validateJWT, async (req: Request, res: Response) => {
       );
     } catch (error) {
       res.status(500).send("Ha habido un error...");
+    }
+  }
+});
+
+clientes.get("/:DNI", validateJWT, async (req: Request, res: Response) => {
+  if (req.body != false) {
+    try {
+      var data = plainToClass(Costumers, req.params, {
+        excludeExtraneousValues: true,
+      });
+      console.log(data);
+      req.body = data;
+      await validate(data);
+      try {
+        connection.query(
+          `SELECT * FROM Cliente WHERE DNI = ?`,
+          [data.Id],
+          (err: any, data: any, fils: any) => {
+            console.log(err);
+            console.log(fils);
+            res.send(data);
+          }
+        );
+      } catch (error) {
+        res.status(500).send("Ha habido un error...");
+      }
+    } catch (err) {
+      res.status(500).send(JSON.stringify(err));
     }
   }
 });
